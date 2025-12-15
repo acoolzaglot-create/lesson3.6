74f6e81dd0b6016d8a83e04941bb83564ea9a0ff
acoolzaglot@gmai.com <acoolzaglot@gmai.com>
2025-12-15 15:25:05 +0300

    Add pytest tests with language parameter

---
+++ test_confitest9.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_add_to_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button is not None, "Кнопка добавления в корзину не найдена"
+
